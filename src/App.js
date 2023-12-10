9849a4be39b97201952f3dc0f4713779d73069ee
Sameer Madhav D <sameermadhav.5@gmail.com>
2023-12-10 08:28:19 +0530

    firdt commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import crypto from "crypto";
 
-function App() {
+import { AppProps } from "next";
+import { LogInWithAnonAadhaar, useAnonAadhaar } from "anon-aadhaar-react";
+import { useEffect } from "react";
+
+import { AnonAadhaarProvider } from "anon-aadhaar-react";
+
+const app_id = BigInt(
+  parseInt(crypto.randomBytes(20).toString("hex"), 16)
+).toString(); // random value.
+
+import { AnonAadhaarProvider } from "anon-aadhaar-react";
+
+
+export default function App({ Component, pageProps }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // Add the Country Identity Provider at the root of your app
+    React.createElement(
+      AnonAadhaarProvider,
+      { _appId: app_id },
+      {_testing: false},
+      React.createElement(Component, pageProps)
+    )
+  );
+}
+
+export default function Home() {
+  const [anonAadhaar] = useAnonAadhaar();
+
+  useEffect(() => {
+    console.log("Anon Aadhaar status: ", anonAadhaar.status);
+  }, [anonAadhaar]);
+
+  return (
+    <div>
+      <LogInWithAnonAadhaar />
+      <p>{anonAadhaar?.status}</p>
     </div>
   );
 }
 
-export default App;
+
